ea3d5b520d84e484fbb73033eaef2b37319b22b4
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-11-16 10:58:05 -0500

    getting the songs to show up in the terminal

--- src/Components/Pages/Index.jsx
+++ src/Components/Pages/Index.jsx
@@ -1,7 +1,10 @@
+import Songs from "../Songs";
+
 export default function Index() {
   return (
     <div className="index-page">
       <h1>Index Page!</h1>
+      <Songs />
     </div>
   );
 }
--- src/Components/Songs.jsx
+++ src/Components/Songs.jsx
@@ -1,23 +1,31 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 const API = import.meta.env.VITE_API_URL;
 
 export default function Songs() {
+  const [songs, setSongs] = useState([]);
 
-    const [songs, setSongs] = useState([]);
-
-    const fetchData = async () =>{
-        try {
-            fetch(`${API}/songs`)
-            .then((res) => res.json())
-            .then((res) =>{
-                setSongs(res);
-            });
-        } catch (error) {
-            return error;
-        }
+  const fetchData = async () => {
+    try {
+      fetch(`${API}/songs`)
+        .then((res) => res.json())
+        .then((res) => {
+          setSongs(res);
+        });
+    } catch (error) {
+      return error;
     }
-    console.log(songs)
+  };
 
-  return <div className="songs"></div>;
+  useEffect(()=>{
+    fetchData();
+  }, [])
+   
+  console.log(songs)
+  return (
+    <div className="songs">
+      <h1>Songs</h1>
+      
+    </div>
+  );
 }
